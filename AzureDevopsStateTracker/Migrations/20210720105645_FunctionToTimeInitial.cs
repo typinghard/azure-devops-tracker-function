fac3ed7de860e8193c83c5863cbd954c4a785b62
Diego Galante <diegogalantte@gmail.com>
2021-07-20 18:51:01 -0300

    Updating | New Migration

---
+++ AzureDevopsStateTracker/Migrations/20210720105645_FunctionToTimeInitial.cs
@@ -0,0 +1,24 @@
+﻿using AzureDevopsStateTracker.Data;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureDevopsStateTracker.Migrations
+{
+    public partial class FunctionToTimeInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var rawFunction = @"CREATE OR ALTER FUNCTION ToTime (@segundos float)
+                                RETURNS TIME
+                                AS BEGIN
+                                    return CONVERT(TIME, DATEADD(SECOND, @segundos + 86400000, 0), 114)
+                                END";
+            migrationBuilder.Sql(rawFunction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var dropFunction = @$"DROP FUNCTION IF EXISTS {DataBaseConfig.SchemaName}.ToTime";
+            migrationBuilder.Sql(dropFunction);
+        }
+    }
+}
